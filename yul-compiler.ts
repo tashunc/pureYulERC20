819221c5d18ed547ce186debc445b3ccdda99d01
tashun <tashun.chamara@gmail.com>
2022-12-11 20:00:22 +0530

    compiler logic refactor

--- yul-compiler.ts
+++ yul-compiler.ts
@@ -3,28 +3,26 @@ const fs = require('fs');
 const YAML = require('yaml');
 
 
-// =================================================================
- export let COMPILER_CONFIG: any = null;
- export let YUL_IGNORE_FILES: any = null;
+// =================== READ CONFIGS ========================================
 
 
-// ==================================================================
-
- export function readYamlConfig() {
-    const loadedConfig = fs.readFileSync("./yul-config.yaml", 'utf8');
-    COMPILER_CONFIG = YAML.parse(loadedConfig)
-    console.log(COMPILER_CONFIG)
+export function readYamlConfig(path: string) {
+    const loadedConfig = fs.readFileSync(path, 'utf8');
+    return YAML.parse(loadedConfig)
+    // console.log(COMPILER_CONFIG)
 }
 
 /**
  * Defines a Yul ignore file to optimize the process and ignore the unnecessary folders
  * */
 
- export function loadYulIgnore(){
-    const contents = fs.readFileSync('.yulignore', 'utf-8');
-    YUL_IGNORE_FILES = contents.split(/\r?\n/);
+export function loadYulIgnore(path: string) {
+    const contents = fs.readFileSync(path, 'utf-8');
+    return contents.split(/\r?\n/);
 
 }
+
+// ================= READ FILE METADATA =================================================
 
 /**
  * input :  contracts, pure_yul_constructor.yul
@@ -33,14 +31,14 @@ const YAML = require('yaml');
  * Given a location to start, search all the files matching the filer and return the name and the relative path
  *
  **/
- export function getAllYulFiles(startPath: string, filter: string) : any {
+export function getAllYulFiles(startPath: string, filter: string, COMPILER_CONFIG: any, YUL_IGNORE_FILES: any): any {
     const found = [];
     const files = fs.readdirSync(startPath);
     for (let i = 0; i < files.length; i++) {
         const filename = path.join(startPath, files[i]);
         const stat = fs.lstatSync(filename);
-        if (stat.isDirectory() && (!YUL_IGNORE_FILES.includes(filename))) {
-            found.push(...getAllYulFiles(filename, filter));
+        if (stat.isDirectory() && (!YUL_IGNORE_FILES || YUL_IGNORE_FILES && !YUL_IGNORE_FILES?.includes(filename))) {
+            found.push(...getAllYulFiles(filename, filter, COMPILER_CONFIG, YUL_IGNORE_FILES));
         } else if (filename.endsWith(filter)) {
             if (files[i].split(".")[0]) {
                 found.push([files[i].split(".")[0], filename]);
@@ -52,39 +50,42 @@ const YAML = require('yaml');
 }
 
 /**
+ * return the file metadata which will be helpful for yul compilation
+ * */
+export function findAFile(startPath: string, name: string, extension: string, COMPILER_CONFIG: any, YUL_IGNORE_FILES: any) {
+    return getAllYulFiles(startPath, name + "." + extension, COMPILER_CONFIG, YUL_IGNORE_FILES)
+
+}
+
+// ===========================================================================================
+/**
  * Given a path and a name, the compiled output of the global yul object will be written to the
  * %arg1%\..\artifact\%arg2%\%arg2%.yaml
  * arg1 - path (convention used here)
  * arg2 - name of the file (convention used here)
  *
  * */
- export function compileToBinaryCode(name: string, path: string , savepath = COMPILER_CONFIG.outputPath) {
-    const fileName = name;
-    require('child_process').exec('cmd /c C:\\addToPath\\yul_pure_compile.bat .\\' + path +  ' ' + name +  savepath,
+export function compileASingleFileToBinaryCode(name: string, path: string, savePath: string) {
+    // console.log('cmd /c C:\\addToPath\\yul_pure_compile.bat ' + path + ' ' + name + ' ' + savePath + '\\' + name);
+    require('child_process').exec('cmd /c C:\\addToPath\\yul_pure_compile.bat ' + path + ' ' + name + ' ' + savePath + '\\' + name,
         (error: any) => {
+
             if (error !== null) {
                 console.log(`exec error: ${error}`);
             }
         });
 }
 
-/**
- * return the file metadata which will be helpful for yul compilation
- * */
- export function findAFile(name: string, extension: string) {
-    return getAllYulFiles("contracts", name + "." + extension)
-
-}
 
 /**
  * Given the root compile all the .yul files
  * */
- export function compileAll() {
-    const file2 = getAllYulFiles(".", ".yul");
-    // console.log(file2)
+export function compileAll(startPath: any, extension: any, COMPILER_CONFIG: any, YUL_IGNORE_FILES: any) {
+    const outputFiles = getAllYulFiles(startPath, extension, COMPILER_CONFIG, YUL_IGNORE_FILES);
+    // console.log(outputFiles)
 
-    for (let i = 0; i < file2.length; i++) {
-        compileToBinaryCode(file2[i][0], file2[i][1]);
+    for (let i = 0; i < outputFiles.length; i++) {
+        compileASingleFileToBinaryCode(outputFiles[i][0], outputFiles[i][1], COMPILER_CONFIG?.savePath);
     }
 }
 
@@ -93,25 +94,33 @@ const YAML = require('yaml');
  * given a file name and an extension a file is compiled
  *
  * */
- export function compileSelected(filename: string, extension: string) {
-    const file2 = findAFile(filename, extension)
-    console.log(file2)
-    if (file2.length > 0) {
-        compileToBinaryCode(file2[0][0], file2[0][1]);
+export function compileSelected(startPath: string, filename: string, extension: string, COMPILER_CONFIG: any, YUL_IGNORE_FILES: any) {
+    const outputFile = findAFile(startPath, filename, extension, COMPILER_CONFIG, YUL_IGNORE_FILES)
+     // console.log(outputFile)
+    if (outputFile.length > 0) {
+        compileASingleFileToBinaryCode(outputFile[0][0], outputFile[0][1], COMPILER_CONFIG?.savePath);
     }
 }
 
- export function getFilteredByteCode(filePath: any) {
+export function getFilteredByteCode(filePath: any) {
     const readByteCode = fs.readFileSync(filePath, 'utf-8')
-    console.log(readByteCode)
+    const lowerBound = readByteCode.indexOf(':',
+        (readByteCode.indexOf('Binary representation:'))) + 1;
+
+    const upperBoundSearch = readByteCode.indexOf('Text representation:')
+    return (upperBoundSearch > -1) ?
+        (YAML.parse(readByteCode.substring(lowerBound, upperBoundSearch))).toString() :
+        (YAML.parse(readByteCode.substring(lowerBound))).toString();
+
 }
 
-/**
- * TEST COMPILATION
- * */
-// readYamlConfig();
-// loadYulIgnore();
-// compileSelected("yulERC20", "yul");
+export function delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// let COMPILER_CONFIG = readYamlConfig();
+// let YUL_IGNORE_FILES =loadYulIgnore();
+// compileSelected("yulERC20", "yul", COMPILER_CONFIG , YUL_IGNORE_FILES);
 // getFilteredByteCode("contracts\\yulERC20.yul");
 // compileAll()
 
